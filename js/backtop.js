4bb148974777dc44f29b374df7840c54ad5d152d
aleng_zhang <aleng_zhang@outlook.com>
2017-07-18 12:47:34 +0800

    Update

--- js/backtop.js
+++ js/backtop.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     // fade in #back-top
     $(function() {
         $(window).scroll(function() {
-            if ($(this).scrollTop() > 400) {
+            if ($(this).scrollTop() > 200) {
                 $('#back-top').fadeIn();
             } else {
                 $('#back-top').fadeOut();
