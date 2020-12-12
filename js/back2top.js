428f8a6bfb088690ce8ac5f8788c824d2a1149d6
aleng <aleng_zhang@outlook.com>
2020-12-12 16:50:04 +0800

    try to fix back2top

--- js/back2top.js
+++ js/back2top.js
@@ -1,12 +1,3 @@
-//当滚动条的位置处于距顶部600像素以下时，跳转链接出现，否则消失
-$(window).scroll(function(){
-    if ($(window).scrollTop()>600){
-        $("#back2top").fadeIn();
-    }else{
-        $("#back2top").fadeOut();
-    }
-});
-
 $(document).ready(function() {
     $("#back2top").click(function(){
         $('body,html').animate({scrollTop:0},500);
